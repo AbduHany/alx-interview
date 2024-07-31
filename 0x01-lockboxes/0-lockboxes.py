ad45f582c192d10c4ccc918f913e65312317c9f0
AbduHany <abdu.hany@gmail.com>
2024-07-31 04:44:27 +0300

    Modify 0-lockboxes.py: Add docstrings

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""Thid module defines the canUnlockAll function
+"""
+
 
 def canUnlockAll(boxes):
+    """This function defines the solution for the lockboxes
+    tech interview problem
+    Args:
+        boxes (List): List of Lists representing a box with each
+        box containing keys.
+    Returns:
+        True - if all boxes can be unlocked
+        False - if not all boxes can be unlocked
+    """
     openedBoxes = [0]
     keys = []
     for key in boxes[0]:

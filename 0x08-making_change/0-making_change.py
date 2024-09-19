925c9e4c614b01afde52a1e736a0437aac4abd7b
AbduHany <abdu.hany@gmail.com>
2024-09-20 01:24:43 +0300

    Change algorithm to greedy algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,22 +13,35 @@ def makeChange(coins, total):
     Returns:
         fewest number of coins needed to meet total
     """
-    n = len(coins)
-    if total <= 0:
-        return 0
-    table = [0 for i in range(total + 1)]
-    table[0] = 0
-    # initializing the table with infinite values
-    for i in range(1, total + 1):
-        table[i] = sys.maxsize
-    # compute minimum coins required
-    for i in range(1, total + 1):
-        # go through smaller coins solutions
-        for j in range(n):
-            if (coins[j] <= i):
-                sub_res = table[i - coins[j]]
-                if sub_res != sys.maxsize and sub_res + 1 < table[i]:
-                    table[i] = sub_res + 1
-    if table[total] == sys.maxsize:
+    coins.sort(reverse=True)  # Sort coins in descending order
+    ans = 0  # To keep track of the number of coins used
+
+    for coin in coins:
+        if total == 0:
+            break  # Stop if the total is met
+        if coin <= total:
+            num_coins = total // coin  # Find how many of this coin can be used
+            ans += num_coins
+            total -= num_coins * coin  # Reduce the total by the coin value
+
+    # If we still have a total left, it's not possible to make that total
+    if total != 0:
         return -1
-    return table[total]
+
+    return ans
+    # table = [0 for i in range(total + 1)]
+    # table[0] = 0
+    # # initializing the table with infinite values
+    # for i in range(1, total + 1):
+    #     table[i] = sys.maxsize
+    # # compute minimum coins required
+    # for i in range(1, total + 1):
+    #     # go through smaller coins solutions
+    #     for j in range(n):
+    #         if (coins[j] <= i):
+    #             sub_res = table[i - coins[j]]
+    #             if sub_res != sys.maxsize and sub_res + 1 < table[i]:
+    #                 table[i] = sub_res + 1
+    # if table[total] == sys.maxsize:
+    #     return -1
+    # return table[total]
